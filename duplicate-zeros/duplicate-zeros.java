43c4a14e445d53ad3b7ea2e21fa8494cc6fee268
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2022-10-16 14:23:10 +0300

    Time: 35 ms, Memory: 45.5 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,24 @@
+class Solution {
+    private void shiftToRight(int[] arr,int ind) {
+        for (int i = arr.length - 1; i >= ind; i--) {
+            if (i > 0) {
+                arr[i] = arr[i - 1];
+            }
+        }
+        return;
+    }
+    
+    public void duplicateZeros(int[] arr) {
+        
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] == 0) {
+                shiftToRight(arr, i);
+                arr[i] = 0;
+                i++;
+            }
+            i++;
+        }
+        return;
+    }
+}
